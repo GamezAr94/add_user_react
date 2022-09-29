47d923667313959a8ba6ee36e700671b0224fdfd
Arturo Gamez <arturogamez51@gmail.com>
2022-09-28 21:37:06 -0700

    removing usestate from form container

--- src/FormContainer/FormContainer.js
+++ src/FormContainer/FormContainer.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from "react";
+import React, { useRef } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import styles from './FormContainer.module.css';
